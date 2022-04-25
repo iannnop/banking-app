22e3f40092a52ada611ef87503a0df324908d76a
Ian <iannnop@gmail.com>
2022-04-25 15:44:33 -0700

    App class with main method for manual testing

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,13 +1,21 @@
 package com.revature;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.revature.account.Account;
+import com.revature.account.AccountDAOImpl;
+import com.revature.transaction.TransactionDAOImpl;
+import com.revature.user.UserDAOImpl;
+import com.revature.user.UserRole;
+import com.revature.user.User;
+
+public class App {
+    public static void main(String[] args) {
+        UserDAOImpl userDAO = new UserDAOImpl();
+        TransactionDAOImpl transactionDAO = new TransactionDAOImpl();
+        User admin = userDAO.getAdmin("admin");
+        System.out.println(admin);
+        Account adminAccount = admin.getAccount(0);
+        System.out.println(adminAccount);
+        adminAccount.deposit(200, "My first paycheck");
+        System.out.println(adminAccount);
     }
 }
