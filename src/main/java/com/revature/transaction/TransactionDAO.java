94f19d9db56bd3a4156e25b623bd108e738ec725
Ian <iannnop@gmail.com>
2022-04-26 13:59:50 -0700

    removed newBalance field and improved updateTransaction

--- src/main/java/com/revature/transaction/TransactionDAO.java
+++ src/main/java/com/revature/transaction/TransactionDAO.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public interface TransactionDAO {
     Transaction getTransaction(int id);
     ArrayList<Transaction> getAllTransactions(int accountId);
-    Transaction createTransaction(int senderId, int receiverId, double amount, TransactionType type, String description, double newBalance);
-    void updateTransaction(Transaction oldTransaction, Transaction newTransaction);
+    Transaction createTransaction(int senderId, int receiverId, double amount, TransactionType type, String description);
+    void updateTransaction(Transaction transaction);
     void deleteTransaction(Transaction transaction);
 }
