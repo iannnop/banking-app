6533947634636d37ac919d44b0d57c4250fbccf7
Ian <iannnop@gmail.com>
2022-04-26 13:57:14 -0700

    renamed getAllAccounts to getUserAccounts

--- src/main/java/com/revature/account/AccountDAO.java
+++ src/main/java/com/revature/account/AccountDAO.java
@@ -9,8 +9,8 @@ public interface AccountDAO {
     Connection connection = ConnectionManager.getConnection();
 
     Account getAccount(int id);
-    ArrayList<Account> getAllAccounts(int userId);
-    Account createAccount(int userId, double startingBalance);
+    ArrayList<Account> getUserAccounts(int userId);
+    Account createAccount(int userId);
     void updateAccount(Account account);
     void deleteAccount(Account account);
 }
