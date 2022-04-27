54c6b3a367c54ca144a14671f3496007f1308084
Ian <iannnop@gmail.com>
2022-04-27 14:15:54 -0700

    added check for closed connection

--- src/test/java/com/revature/jdbc/ConnectionManagerTest.java
+++ src/test/java/com/revature/jdbc/ConnectionManagerTest.java
@@ -3,14 +3,17 @@ package com.revature.jdbc;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 public class ConnectionManagerTest {
 
     @Test
-    public void connectionTest() {
+    public void connectionTest() throws SQLException {
         Connection connection = ConnectionManager.getConnection();
         assertNotNull(connection);
+        assertFalse(connection.isClosed());
     }
 }
