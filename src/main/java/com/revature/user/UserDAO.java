e44a6f7763e87223b4aaaa18bdbd08a71fdeb477
Ian <iannnop@gmail.com>
2022-04-27 14:20:46 -0700

    added getUser methods with id and username

--- src/main/java/com/revature/user/UserDAO.java
+++ src/main/java/com/revature/user/UserDAO.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public interface UserDAO {
 
+    User getUser(int id);
+    User getUser(String username);
     User getCustomer(String username);
     User getEmployee(String username);
     User getAdmin(String username);
