daf0c9f2fff4c33a750c56a0a93e06f58df39bc6
Ian <iannnop@gmail.com>
2022-04-27 14:17:11 -0700

    added exceptions for invalid input and negative balance

--- src/main/java/com/revature/account/Account.java
+++ src/main/java/com/revature/account/Account.java
@@ -1,16 +1,16 @@
 package com.revature.account;
 
+import com.revature.exception.InvalidInputException;
 import com.revature.exception.NegativeBalanceException;
 import com.revature.transaction.Transaction;
 import com.revature.transaction.TransactionDAOImpl;
 import com.revature.transaction.TransactionType;
 
+import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.List;
 
-public class Account {
-    private static final AccountDAOImpl accountDAO = new AccountDAOImpl();
-    private static final TransactionDAOImpl transactionDAO = new TransactionDAOImpl();
+public class Account implements Serializable {
 
     private final int id;
     private final Timestamp accountCreated;
@@ -23,6 +23,7 @@ public class Account {
     * Account constructor with all required fields
     * */
     public Account(int id, Timestamp accountCreated, AccountStatus status, double balance) {
+        TransactionDAOImpl transactionDAO = new TransactionDAOImpl();
         this.id = id;
         this.accountCreated = accountCreated;
         this.status = status;
@@ -34,6 +35,7 @@ public class Account {
     * Account constructor with all fields
     * */
     public Account(int id, Timestamp accountCreated, AccountStatus status, double balance, String description) {
+        TransactionDAOImpl transactionDAO = new TransactionDAOImpl();
         this.id = id;
         this.accountCreated = accountCreated;
         this.status = status;
@@ -93,25 +95,43 @@ public class Account {
                 '}';
     }
 
-    public void deposit(double amount, String description) {
+    public void deposit(double amount, String description) throws InvalidInputException {
+        if (amount <= 0) {
+            throw new InvalidInputException("Deposit amount cannot be less than or equal to 0");
+        }
+        AccountDAOImpl accountDAO = new AccountDAOImpl();
+        TransactionDAOImpl transactionDAO = new TransactionDAOImpl();
+
         balance += amount;
         transactions.add(transactionDAO.createTransaction(0, id, amount, TransactionType.DEPOSIT, description));
         accountDAO.updateAccount(this);
     }
 
-    public void withdraw(double amount, String description) throws NegativeBalanceException {
-        if (balance - amount < 0) {
-            throw new NegativeBalanceException("ERROR"); //TODO Add error message
+    public void withdraw(double amount, String description) throws NegativeBalanceException, InvalidInputException {
+        if (amount <= 0) {
+            throw new InvalidInputException("Withdraw amount cannot be less than or equal to 0");
         }
+        if (balance - amount < 0) {
+            throw new NegativeBalanceException("Withdraw amount is greater than available balance");
+        }
+        AccountDAOImpl accountDAO = new AccountDAOImpl();
+        TransactionDAOImpl transactionDAO = new TransactionDAOImpl();
+
         balance -= amount;
         transactions.add(transactionDAO.createTransaction(id, 0, amount, TransactionType.WITHDRAWAL, description));
         accountDAO.updateAccount(this);
     }
 
-    public void transfer(Account receiver, double amount, String description) throws NegativeBalanceException {
-        if (balance - amount < 0) {
-            throw new NegativeBalanceException("ERROR MESSAGE"); //TODO Add error message
+    public void transfer(Account receiver, double amount, String description)
+            throws NegativeBalanceException, InvalidInputException {
+        if (amount <= 0) {
+            throw new InvalidInputException("Transfer amount cannot be less than or equal to 0");
         }
+        if (balance - amount < 0) {
+            throw new NegativeBalanceException("Transfer amount is greater than available balance");
+        }
+        TransactionDAOImpl transactionDAO = new TransactionDAOImpl();
+
         Transaction transaction = transactionDAO.createTransaction(id, receiver.id, amount, TransactionType.TRANSFER, description);
         balance -= amount;
         transactions.add(transaction);
