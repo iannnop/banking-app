d835b1148808e18b87b146cfffab1cb2035f5768
Ian <iannnop@gmail.com>
2022-05-01 13:22:59 -0700

    created Javalin REST API Server

--- src/main/java/com/revature/Server.java
+++ src/main/java/com/revature/Server.java
@@ -1,7 +1,14 @@
 package com.revature;
 
+import com.revature.account.AccountController;
+import com.revature.transaction.TransactionController;
+import com.revature.user.UserController;
 import io.javalin.Javalin;
 
 public class Server {
     Javalin app = Javalin.create().start(7070);
+
+    UserController userController = new UserController(app);
+    AccountController accountController = new AccountController(app);
+    TransactionController transactionController = new TransactionController(app);
 }
